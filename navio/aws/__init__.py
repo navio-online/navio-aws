48c9bfa066b0b2209618e0bcfd85d7e40297c7e0
Peter Salnikov <peter@salnikov.cc>
2017-12-19 08:10:19 +0300

    add generatePassword to export

--- navio/aws/__init__.py
+++ navio/aws/__init__.py
@@ -13,8 +13,12 @@ from navio.aws.services._cloudformation import AWSCloudFormation
 from navio.aws.services._lambda import AWSLambda
 from navio.aws.services._s3 import AWSS3
 from navio.aws.services._cloudfront import AWSCloudFront
+from navio.aws._common import generatePassword
 
 import pkgutil
 __path__ = pkgutil.extend_path(__path__,__name__)
 
-__all__ = ['AWSLambda', 'AWSCloudFormation', 'AWSIAM', 'AWSS3', 'AWSCloudFront', 'AWSSession']
+__all__ = [
+  'AWSLambda', 'AWSCloudFormation', 'AWSIAM', 'AWSS3', 'AWSCloudFront', 'AWSSession', 
+  'generatePassword'
+]
