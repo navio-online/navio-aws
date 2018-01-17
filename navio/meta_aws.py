f11e19de02bbd123bb30eb3acb243d5f7f7b7e2b
Peter Salnikov <peter@salnikov.cc>
2018-01-17 14:43:01 +0300

    Version updated to 0.1.29

--- navio/meta_aws.py
+++ navio/meta_aws.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.28"
+__version__ = "0.1.29"
 __license__ = "MIT License"
 __website__ = "https://oss.navio.tech/navio-aws/"
 __download_url__ = ('https://github.com/naviotech/navio-aws/archive'
