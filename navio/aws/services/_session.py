bde239d3ff91f8df1227f7ccf77cf00a10e30188
Peter Salnikov <peter@salnikov.cc>
2018-10-02 21:39:17 +0300

    make AWSSession shared for current thread

--- navio/aws/services/_session.py
+++ navio/aws/services/_session.py
@@ -1,5 +1,5 @@
 import boto3
-import botocore
+import threading
 
 
 class AWSSession(object):
@@ -7,18 +7,33 @@ class AWSSession(object):
     def __init__(self, profile_name, region_name=None):
         self.profile_name = profile_name
         self.region_name = region_name
+        self._thread_local = threading.local()
+        self._thread_local.boto3_sessions = dict()
 
-        if profile_name in boto3.session.Session().available_profiles:
-            if region_name:
-                self.session = boto3.session.Session(
-                    profile_name=self.profile_name,
-                    region_name=self.region_name
-                )
-            else:
-                self.session = boto3.session.Session(
-                    profile_name=self.profile_name)
+    def __session(self):
+        sessions = self._thread_local.boto3_sessions
+        session_key = '{profile}.{region}'.format(
+            profile=self.profile_name,
+            region=self.region_name
+          )
+
+        if session_key not in sessions:
+            if self.profile_name in boto3.session.Session().available_profiles:
+                if self.region_name:
+                    sessions[session_key] = boto3.session.Session(
+                        profile_name=self.profile_name,
+                        region_name=self.region_name
+                    )
+                else:
+                    sessions[session_key] = boto3.session.Session(
+                        profile_name=self.profile_name)
         else:
             raise Exception('Profile not available: {}'.format(profile_name))
 
+        return sessions[session_key]
+
     def client(self, name):
-        return self.session.client(name)
+        return self.__session().client(name)
+
+    def resource(self, name):
+        return self.__session().resource(name)
