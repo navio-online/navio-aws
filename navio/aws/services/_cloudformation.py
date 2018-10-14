a39010ea80cf8d946a5d56ba39cc304caf6285a7
Peter Salnikov <peter@salnikov.cc>
2018-10-14 10:33:34 +0300

    add serverless flag, remove on_failure obligatory

--- navio/aws/services/_cloudformation.py
+++ navio/aws/services/_cloudformation.py
@@ -41,7 +41,10 @@ class AWSCloudFormation(AWSSession):
         else:
             self.easy_service = False
 
-            self.on_failure = kwargs.get('on_failure', 'ROLLBACK')
+            self.on_failure = kwargs.get('on_failure', 'DELETE')
+            self.serverless = kwargs.get('serverless', False)
+            raise Exception('serveless argument should be bool') if type(self.serverless) is not bool
+
 
             if 'template' in kwargs and type(kwargs['template']) == str:
                 self.template = kwargs['template']
