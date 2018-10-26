e59a3770cd0de276cb85808cc6aad95e835232f9
Peter Salnikov <peter@salnikov.cc>
2018-10-26 11:16:38 +0300

    chage invalidation function signature

--- navio/aws/services/_cloudfront.py
+++ navio/aws/services/_cloudfront.py
@@ -18,15 +18,15 @@ class AWSCloudFront(AWSSession):
         super(self.__class__, self).__init__(kwargs['profile_name'])
         self.distribution_id = kwargs.get('distribution_id')
 
-    def invalidate(self, **kwargs):
+    def invalidate(self, *files):
         cloudfront = self.client('cloudfront')
         resp = cloudfront.create_invalidation(
             DistributionId=self.distribution_id,
             InvalidationBatch={
                 'CallerReference': uuid.uuid4().hex,
                 'Paths': {
-                    'Quantity': len(kwargs.get('files')),
-                    'Items': kwargs.get('files')
+                    'Quantity': len(files),
+                    'Items': files
                 }
             }
         )
