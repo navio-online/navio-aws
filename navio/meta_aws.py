e348062484e1e0dfb523280149a169b9abeaba79
Peter Salnikov <peter@salnikov.cc>
2020-01-10 16:30:55 +0700

    Version updated to 0.1.77

--- navio/meta_aws.py
+++ navio/meta_aws.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.76"
+__version__ = "0.1.77"
 __license__ = "MIT License"
 __website__ = "https://oss.navio.tech/navio-aws/"
 __download_url__ = ('https://github.com/naviotech/navio-aws/archive'
